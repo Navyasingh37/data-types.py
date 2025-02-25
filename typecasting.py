b0268f062a7017f6b97e7c2dd0d8dcfe129f0674
Navyasingh37 <Singhchanda346@gmail.com>
2025-02-25 20:39:55 +0545

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,23 @@
+name="Navya"
+age= 9
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data Type of name is",type(name))
+
+print("Age :", age)
+print("Data Type of Age is",type(age))
+
+print("is_student :",is_student)
+print("Data Type of is_student is",type(is_student))
+
+print("weight:",weight)
+print("Data Type of weight is",type(weight))
+
+print("\n After Type Casting....")
+print(age)
+print("Data Type of Age is",type(age))
+weight - int(weight)
+print(weight)
+print("Data Type of weight is",type(weight))
